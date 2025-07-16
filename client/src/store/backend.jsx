b356c6bb7d7b07b15ae27220dcd76067d323c2af
Debanjan Paul <debanjanpaul607@gmail.com>
2025-07-16 18:50:04 +0530

    Updated 6.00PM 16/07/2025

--- client/src/store/backend.jsx
+++ client/src/store/backend.jsx
@@ -1,3 +1,4 @@
 // Centralized backend URL for API requests
 //export const BACKEND_URL = 'http://localhost:8000'; 
-export const BACKEND_URL = 'https://excel-analytics-1-fehm.onrender.com'; 
+export const BACKEND_URL = 'https://excel-analytics-ljd3.onrender.com'; 
+//export const BACKEND_URL = 'https://excel-analytics-1-fehm.onrender.com';
